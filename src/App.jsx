c7ea03f96ed731c45a71b81c4012914d0abaab68
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-05-07 09:08:38 +0100

    data send to database

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,18 @@ function App() {
     const email = form.email.value;
     const user = { name, email }
     // console.log(user);
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
   }
   return (
     <>

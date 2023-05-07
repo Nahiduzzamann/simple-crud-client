3b6ed9235449528e907f93c8da6d231c7625f099
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-05-07 08:40:02 +0100

    create form done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleAddUser = event => {
+    event.preventDefault()
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+    // console.log(user);
+  }
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' id='' /><br />
+        <input type="email" name='email' id='' /><br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   )
 }
